adb59ca96e2cb4bd78ac1d518122b0088808dbdf
GiorCocc <giorcocc@gmail.com>
2022-05-15 15:39:10 +0200

    fatto esercizio 7 e 8 esercitazione 4

--- Esercitazione4/es7.c
+++ Esercitazione4/es7.c
@@ -8,12 +8,12 @@ int main()
 {
   int status; // variabile per memorizzare lo stato di terminazione del processo
   pid_t pid;  // pid del processo figlio
-  char *arg[] = {"ls", "-l", "/usr/include", (char *)0};
+  char *arg[] = {"ls", "-l", "/usr/include", (char *)0}; // argomenti da passare al processo figlio (mostra la lista dei file di /usr/include)
 
   if ((pid = fork()) == 0)
   {
     /* CODICE ESEGUITO DAL FIGLIO */
-    execv("/bin/ls", arg);
+    execv("/bin/ls", arg); // cambio dell'ambiente di esecuzione "/bin/ls" con argomenti di invocazione "ls -l /usr/include" contenuti in arg
     /* Si torna solo in caso di errore */
     exit(1);
   }
--- Esercitazione4/es8.c
+++ Esercitazione4/es8.c
@@ -4,17 +4,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
   int status;
   pid_t pid;
-  char *env[] = {"TERM=vt100", "PATH=/bin:/usr/bin", (char *)0};
-  char *args[] = {"cat", "f1", "f2", (char *)0};
+  char *env[] = {"TERM=vt100", "PATH=/bin:/usr/bin", (char *)0}; // variabili di ambiente da passare al processo figlio
+
+  if(argc!=3)
+  {
+    printf("Usage: %s <nome file f1> <nome file f2>\n", argv[0]);
+    exit(1);
+  }
+
+  char *args[] = {"cat", argv[1], argv[2], (char *)0}; // argomenti da passare al processo figlio (mostra i file argv[1] e argv[2])
 
   if ((pid = fork()) == 0)
   {
     /* CODICE ESEGUITO DAL FIGLIO */
-    execve("/bin/cat", args, env);
+    execve("/bin/cat", args, env); // cambio dell'ambiente di esecuzione "/bin/cat" con argomenti di invocazione "cat argv[1] argv[2]" contenuti in args e env
     /* Si torna solo in caso di errore */
     exit(1);
   }
